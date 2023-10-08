ee3b72e6a75602cd7b77f38c146c66462ff1eb10
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-08 11:39:05 -0300

    chore: add os_mon in extra_applications

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Exmeal.MixProject do
   def application do
     [
       mod: {Exmeal.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
