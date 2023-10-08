15bcbc80b3deec0122eb3619690adc41129b229d
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-08 13:28:17 -0300

    chore: add json encoder in meal schema

--- lib/exmeal/meal.ex
+++ lib/exmeal/meal.ex
@@ -5,6 +5,8 @@ defmodule Exmeal.Meal do
 
   @required_params [:calories, :date, :description]
 
+  @derive {Jason.Encoder, only: [:id, :calories, :date, :description, :inserted_at, :updated_at]}
+
   schema "meals" do
     field :calories, :integer
     field :date, :date
