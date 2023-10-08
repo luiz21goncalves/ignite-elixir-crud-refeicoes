0a8cfe8da0abb186dace3069acaf15ec2531d8c7
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-08 11:47:48 -0300

    chore: add credo dependency

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Exmeal.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:ecto_psql_extras, "~> 0.6"}
+      {:ecto_psql_extras, "~> 0.6"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
