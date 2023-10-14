821ceb0c5d4bdc54f1666d4c63c6896e226c2740
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-14 11:03:02 -0300

    chore: update delete meal

--- test/exmeal/meals/delete_test.exs
+++ test/exmeal/meals/delete_test.exs
@@ -1,24 +1,29 @@
 defmodule Exmeal.Meals.DeleteTest do
   use Exmeal.DataCase, async: true
 
+  import Exmeal.Factory
+
+  alias Exmeal.{Meal, User}
+
   describe "call/1" do
     test "when a valid id is given, returns the meal" do
-      params = %{
-        calories: 20,
-        date: ~D[2001-05-02],
-        description: "Banana"
-      }
+      user_params = build(:user_params)
 
-      {_ok, meal} = Exmeal.create_meal(params)
+      {:ok, %User{id: user_id}} = Exmeal.create_user(user_params)
 
-      response = Exmeal.delete_meal(meal.id)
+      params = build(:meal_params, %{user_id: user_id})
+
+      {:ok, %Meal{id: id}} = Exmeal.create_meal(params)
+
+      response = Exmeal.delete_meal(id)
 
       assert {:ok,
-              %Exmeal.Meal{
+              %Meal{
                 calories: 20,
                 date: ~D[2001-05-02],
                 description: "Banana",
-                id: _id
+                id: ^id,
+                user_id: ^user_id
               }} = response
     end
 
