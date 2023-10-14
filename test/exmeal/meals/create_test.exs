1cba1ec45ce2762a1d25d2d65bf5be577bf6d4e0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-14 11:00:07 -0300

    test: update create meal

--- test/exmeal/meals/create_test.exs
+++ test/exmeal/meals/create_test.exs
@@ -1,23 +1,27 @@
 defmodule Exmeal.Meals.CreateTest do
   use Exmeal.DataCase, async: true
 
+  import Exmeal.Factory
+
+  alias Exmeal.{Meal, User}
+
   describe "call/1" do
     test "when all params are valid, returns the meal" do
-      params = %{
-        calories: 20,
-        date: ~D[2001-05-02],
-        description: "Banana"
-      }
+      user_params = build(:user_params)
 
-      response = Exmeal.create_meal(params)
+      {:ok, %User{id: user_id}} = Exmeal.create_user(user_params)
 
-      assert {:ok,
-              %Exmeal.Meal{
-                calories: 20,
-                date: ~D[2001-05-02],
-                description: "Banana",
-                id: _id
-              }} = response
+      params = build(:meal_params, %{user_id: user_id})
+
+      {:ok, %Meal{id: id} = response} = Exmeal.create_meal(params)
+
+      assert %Meal{
+               calories: 20,
+               date: ~D[2001-05-02],
+               description: "Banana",
+               id: ^id,
+               user_id: ^user_id
+             } = response
     end
 
     test "when there are invalid params, returns an error" do
@@ -28,7 +32,7 @@ defmodule Exmeal.Meals.CreateTest do
 
       response = Exmeal.create_meal(params)
 
-      expected_response = %{description: ["can't be blank"]}
+      expected_response = %{description: ["can't be blank"], user_id: ["can't be blank"]}
 
       assert {:error, %Exmeal.Error{status: :bad_request, result: changeset}} = response
       assert errors_on(changeset) == expected_response
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exmeal.Factory do
   use ExMachina.Ecto, repo: Exmeal.Repo
 
-  alias Exmeal.User
+  alias Exmeal.{Meal, User}
 
   def user_params_factory do
     %{name: "John Doe", email: "john.doe@email.com", document: "12345678910"}
@@ -13,6 +13,25 @@ defmodule Exmeal.Factory do
       email: "john.doe@email.com",
       document: "12345678910",
       id: "69961117-d966-4e2f-ac55-476d4f78dddf"
+    }
+  end
+
+  def meal_params_factory do
+    %{
+      description: "Banana",
+      date: "2001-05-02",
+      calories: "20",
+      user_id: "388fccf4-f3bb-4822-9817-973a62158d20"
+    }
+  end
+
+  def meal_factory do
+    %Meal{
+      description: "banana",
+      date: "2001-05-02",
+      calories: "2",
+      user_id: "388fccf4-f3bb-4822-9817-973a62158d20",
+      id: "69961117-d966-4e2f-ac55-476d4f78ddfa"
     }
   end
 end
